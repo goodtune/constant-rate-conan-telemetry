d8da50ae3294d9f41564d72aa1098bf5cd8cc5ed
Gary Reynolds <gary.reynolds@touchtechnology.com.au>
2022-08-23 22:23:25 +1000

    swap to fmt::print from std:cout

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include "Poco/MD5Engine.h"
 #include "Poco/DigestStream.h"
-#include <iostream>
+#include <fmt/core.h>
 
 int main(int argc, char **argv) {
     Poco::MD5Engine md5;
     Poco::DigestOutputStream ds(md5);
     ds << "abcdefghijklmnopqrstuvwxyz";
     ds.close();
-    std::cout << Poco::DigestEngine::digestToHex(md5.digest()) << std::endl;
+    fmt::print("{}\n", Poco::DigestEngine::digestToHex(md5.digest()));
     return 0;
 }
